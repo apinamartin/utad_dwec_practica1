34d1e52a0acb10b637608b2464a88409c37a942b
apinamartin <apinamartin@gmail.com>
2024-12-07 13:38:55 +0100

    Ignorar la carpeta src/environments

--- src/environments/environment.developer.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'https://api.themoviedb.org/3/movie/',
-    posterImageUrl: 'https://image.tmdb.org/t/p/w500',
-    backdropImageUrl: 'https://image.tmdb.org/t/p/w1280',
-    genresUrl: 'https://api.themoviedb.org/3/genre/movie/list',
-    api_key: 'ed418be8c101a8fa9165e9af5509e8c1'
-};
